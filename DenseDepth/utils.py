5b6677e7d8b03a452613de4419cb5436d72fc2ef
Kanukollu, Suman <Suman.Kanukollu@harman.com>
2020-05-10 15:05:58 +0530

    Updated print statement to know the batch size

--- DenseDepth/utils.py
+++ DenseDepth/utils.py
@@ -22,6 +22,7 @@ def predict(model, images, minDepth=10, maxDepth=1000, batch_size=20):
     if len(images.shape) < 3: images = np.stack((images,images,images), axis=2)
     if len(images.shape) < 4: images = images.reshape((1, images.shape[0], images.shape[1], images.shape[2]))
     # Compute predictions
+    print('At batch sz : {}'.format(batch_size))
     predictions = model.predict(images, batch_size=batch_size)
     # Put in expected range
     return np.clip(DepthNorm(predictions, maxDepth=maxDepth), minDepth, maxDepth) / maxDepth
